8d9793f002e1d4544e363c7aae7156fe54da3f25
Ta Quang Vinh <tquangvinh.ed@gmail.com>
2026-02-01 23:22:21 +0700

    first commit

---
+++ src/main/java/com/example/otp_validation/OtpValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.otp_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtpValidationApplication.class, args);
+	}
+
+}
